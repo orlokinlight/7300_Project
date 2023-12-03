1d541e9b803b0100f0cd5f84b6422736bdfd4086
orlokinlight <82125381+orlokinlight@users.noreply.github.com>
2023-12-03 10:45:41 -0600

    Update BWT.cpp

--- BWT.cpp
+++ BWT.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 
 // Function to perform Burrows-Wheeler Transform
-static std::string bwtTransform(std::vector<int>& suffixArray, std::string& originalString) {
+static std::vector<int> bwtTransform(std::vector<int>& suffixArray, std::string& originalString) {
     int n = suffixArray.size();
-    std::string transformedString;
+    std::vector<int> transformedArray;
 
     // Construct the Burrows-Wheeler Transform
     for (int i = 0; i < n; ++i) {
@@ -13,25 +13,12 @@ static std::string bwtTransform(std::vector<int>& suffixArray, std::string& orig
         if (index < 0) {
             index += n;  // Adjust for the sentinel character
         }
-        transformedString += originalString[index];
+        transformedArray.push_back(originalString[index]);
     }
 
-    return transformedString;
+    return transformedArray;
 }
 
-/*int main() {
-    // Example suffix array generated from the previous code
-    std::vector<int> suffixArray = {5, 3, 1, 0, 4, 2};
 
-    // Corresponding original string
-    std::string originalString = "banana\0\0";
 
-    // Perform Burrows-Wheeler Transform
-    std::string bwtResult = bwtTransform(suffixArray, originalString);
 
-    // Print the result
-    std::cout << "Original String: " << originalString << std::endl;
-    std::cout << "Burrows-Wheeler Transform: " << bwtResult << std::endl;
-
-    return 0;
-}*/

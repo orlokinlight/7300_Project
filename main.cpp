b3b9bc1cbd423bdf1a5adf8c3e214ab5379923aa
orlokinlight <82125381+orlokinlight@users.noreply.github.com>
2023-12-03 08:33:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <SA.cpp>
+#include "SA.cpp"
+#include "BWT.cpp"
 
-using namespace std;
+//using namespace std;
 
 int main() {
-    // Example usage
-    std::string input = "banana";
-    int n = input.size();
+    
+    // Replace "your_file_path" with the actual path to the file on your desktop
+    std::string file_path = "/Users/josephbrown/Desktop/sample3.txt";
 
-    SA sa1;
+    // Open the file
+    std::ifstream file(file_path);
+
+    // Check if the file is opened successfully
+    if (!file.is_open()) {
+        std::cerr << "Error opening the file." << std::endl;
+        return 1;
+    }
+
+    // Read the file contents into a string
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    std::string input = buffer.str();
+
+    // Close the file
+    file.close();
+
+    // Display the contents of the file
+    std::cout << "File Contents:\n" << input << std::endl;
+    
+    
+    int n = input.size();
     
     // Append sentinel characters
     input += '\0';
@@ -24,7 +49,7 @@ int main() {
 
     // Construct the suffix array using the provided function
     std::vector<int> sa(n);
-    sa1.suffixArray(s.data(), sa.data(), n, 255);
+    suffixArray(s.data(), sa.data(), n, 255);
 
     // Print the result
     std::cout << "Suffix Array: ";
@@ -32,6 +57,15 @@ int main() {
         std::cout << index << " ";
     }
     std::cout << std::endl;
+    
+    // Perform Burrows-Wheeler Transform
+    std::string bwtResult = bwtTransform(sa, input);
+
+    // Print the result
+    //std::cout << "Original String: " << input << std::endl;
+    std::cout << "Burrows-Wheeler Transform: " << bwtResult << std::endl;
 
     return 0;
 }
+
+
